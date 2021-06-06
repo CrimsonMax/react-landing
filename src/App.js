b1494a935aebe6ac14984eea426e2e0f0d92604c
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2021-06-06 03:18:29 +0300

    setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="bord">
+          <div className="bar"></div>
+
+        </div>
+        <div className="wrapper">
+          <h1>Hello!</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/util/icons.js
@@ -0,0 +1,12 @@
+{/* 
+  
+<i class="far fa-arrow-alt-circle-left"></i>
+<i class="far fa-arrow-alt-circle-right"></i>
+<i class="far fa-envelope"></i>
+<i class="fab fa-vk"></i>
+<i class="fab fa-facebook-f"></i>
+<i class="fab fa-twitter"></i>
+<i class="fab fa-instagram"></i>
+<i class="fas fa-globe-americas"></i> 
+
+*/}
